7d49c57c5f671d9fdc727ed0a284c374a27343fc
mariusponct <spider35maryus@gmail.com>
2024-05-30 10:18:43 +0300

    update entire app, make app more user-friendly

--- app.py
+++ app.py
@@ -2,45 +2,60 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def calculate_resistance(band1, band2, band3, band4=None):
-    # Color code dictionary mapping colors to their respective values
-    color_codes = {
-        'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4,
-        'green': 5, 'blue': 6, 'violet': 7, 'gray': 8, 'white': 9
-    }
-    
-    # Mapping the multiplier to its corresponding power of 10
-    multipliers = {'black': 1, 'brown': 10, 'red': 100, 'orange': 1000, 'yellow': 10000,
-                   'green': 100000, 'blue': 1000000, 'violet': 10000000, 'gray': 100000000, 'white': 1000000000,
-                   'gold': 0.1, 'silver': 0.01}
-    
-    # Mapping the tolerance to its corresponding percentage value
-    tolerances = {'brown': '+/- 1%', 'red': '+/- 2%', 'green': '+/- 0.5%', 'blue': '+/- 0.25%', 'violet': '+/- 0.1%',
-                  'gray': '+/- 0.05%', 'gold': '+/- 5%', 'silver': '+/- 10%'}
-    
-    # Calculate resistance based on color bands
-    resistance = (color_codes[band1] * 10 + color_codes[band2]) * multipliers[band3]
-    
-    # If there is a 4th band (tolerance), calculate and append the tolerance
-    if band4:
-        resistance_str = f"{resistance} Ohms, {tolerances[band4]}"
-    else:
-        resistance_str = f"{resistance} Ohms"
-    
-    return resistance_str
+# Define the color code dictionary
+color_codes = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'gray': 8,
+    'white': 9
+}
 
-@app.route('/')
+# Define tolerance codes
+tolerance_codes = {
+    'brown': 1,
+    'red': 2,
+    'green': 0.5,
+    'blue': 0.25,
+    'violet': 0.1,
+    'gray': 0.05,
+    'gold': 5,
+    'silver': 10
+}
+
+# Function to calculate resistance
+def calculate_resistance(band1, band2, band3, multiplier, tolerance):
+    try:
+        digit1 = color_codes[band1]
+        digit2 = color_codes[band2]
+        digit3 = color_codes[band3]
+        multiplier_value = 10 ** color_codes[multiplier]
+        tolerance_value = tolerance_codes.get(tolerance, None)
+        resistance = (digit1 * 100 + digit2 * 10 + digit3) * multiplier_value
+        return resistance, tolerance_value
+    except KeyError:
+        return None, None
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    band1 = request.form.get('band1')
-    band2 = request.form.get('band2')
-    band3 = request.form.get('band3')
-    band4 = request.form.get('band4')  # Optional, tolerance band
-    resistance = calculate_resistance(band1, band2, band3, band4)
-    return render_template('result.html', resistance=resistance)
+    resistance = None
+    tolerance_value = None
+    band1 = band2 = band3 = multiplier = tolerance = None
+    if request.method == 'POST':
+        band1 = request.form.get('band1')
+        band2 = request.form.get('band2')
+        band3 = request.form.get('band3')
+        multiplier = request.form.get('multiplier')
+        tolerance = request.form.get('tolerance')
+        resistance, tolerance_value = calculate_resistance(band1, band2, band3, multiplier, tolerance)
+    return render_template('index.html', resistance=resistance, tolerance_value=tolerance_value,
+                           band1=band1, band2=band2, band3=band3, multiplier=multiplier, tolerance=tolerance,
+                           color_codes=color_codes, tolerance_codes=tolerance_codes)
 
 if __name__ == '__main__':
     app.run(debug=True)
